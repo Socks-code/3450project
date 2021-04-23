c96d885f533aa89fff32daf7a73332dfe995c0dd
Socks-code <55949458+Socks-code@users.noreply.github.com>
2021-04-23 11:26:18 -0400

    Add files via upload

---
+++ Phase 3/home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<head>
+</head>
+
+<body>
+    <nav class="nav">
+    
+    <li class="nav-item">
+        <a class="href"></a>
+        <h1>Admin</h1>
+        </li>
+        
+        <li class="nav-item">
+        <a class="href"></a>
+        <h1>Product Information</h1>
+        </li>
+    
+    
+    
+    
+    </nav>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/owldb/index.htm"><img src="/owldb/images/overwatch_league_logo.png" alt="Logo" style="width:50px;"></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/owldb/teams.php">
+                    <h1 style="text-align:justify">Teams</h1>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/owldb/players.php">
+                    <h1>Players</h1>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/owldb/characters.php">
+                    <h1>Characters</h1>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/owldb/maps.php">
+                    <h1>Maps</h1>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/owldb/login.html">
+                    <h1>Admin</h1>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    </body>
+    </html>
